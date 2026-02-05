2dd0898449d806d2a63c224ef0ac52e994d4e653
silvi-06 <shahsilvi06@gmail.com>
2026-02-05 14:46:52 +0530

    database.ts change

--- Backend/backend/config/database.ts
+++ Backend/backend/config/database.ts
@@ -1,28 +1,25 @@
 export default ({ env }) => {
-  const client = env('DATABASE_CLIENT');
-
-  if (client !== 'postgres') {
-    throw new Error('Only postgres is allowed');
-  }
-
-  const isSSL =
-    env('DATABASE_SSL', 'false') === 'true';
+  const client = env('DATABASE_CLIENT', 'postgres');
 
   return {
     connection: {
-      client: 'postgres',
+      client,
       connection: env('DATABASE_URL')
         ? {
           connectionString: env('DATABASE_URL'),
-          ssl: isSSL ? { rejectUnauthorized: false } : false,
+          ssl: env.bool('DATABASE_SSL', false)
+            ? { rejectUnauthorized: false }
+            : false,
         }
         : {
-          host: env('DATABASE_HOST'),
+          host: env('DATABASE_HOST', 'localhost'),
           port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME'),
-          user: env('DATABASE_USERNAME'),
-          password: env('DATABASE_PASSWORD'),
-          ssl: isSSL ? { rejectUnauthorized: false } : false,
+          database: env('DATABASE_NAME', 'strapi'),
+          user: env('DATABASE_USERNAME', 'strapi'),
+          password: env('DATABASE_PASSWORD', 'strapi'),
+          ssl: env.bool('DATABASE_SSL', false)
+            ? { rejectUnauthorized: false }
+            : false,
         },
       pool: { min: 2, max: 10 },
     },
